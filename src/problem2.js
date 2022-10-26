a8251c73a617cdcf147b0e823b3827d12de69d84
Moomi98 <kymkjh2002@gmail.com>
2022-10-26 16:50:07 +0900

    problem2 해결

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,30 @@
+function getNotDuplicatedCryptogram(cryptogram) {
+  const newCryptogram = [cryptogram[0]];
+
+  for (let i = 1; i < cryptogram.length; i++) {
+    const target = newCryptogram[newCryptogram.length - 1];
+    if (target !== cryptogram[i]) {
+      newCryptogram.push(cryptogram[i]);
+    } else {
+      newCryptogram.pop();
+    }
+  }
+
+  return newCryptogram.join("");
+}
+
 function problem2(cryptogram) {
   var answer;
+
+  while (true) {
+    const newCryptogram = getNotDuplicatedCryptogram(cryptogram);
+    if (newCryptogram === cryptogram) {
+      answer = newCryptogram;
+      break;
+    }
+    cryptogram = newCryptogram;
+  }
+
   return answer;
 }
 
