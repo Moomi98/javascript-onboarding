97d87cab9325af6a35abcec1f0aed43013719e20
Moomi98 <kymkjh2002@gmail.com>
2022-10-26 16:28:21 +0900

    problem1 해결

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,41 @@
+function getSum(number) {
+  number = number.toString().split("").map(Number);
+
+  return number.reduce((acc, cur) => acc + cur);
+}
+
+function getMultiple(number) {
+  number = number.toString().split("").map(Number);
+
+  return number.reduce((acc, cur) => acc * cur);
+}
+
+function getHighestNumber(pages) {
+  const [leftPage, rightPage] = pages;
+  const highestLeftPage = Math.max(getSum(leftPage), getMultiple(leftPage));
+  const highestRightPage = Math.max(getSum(rightPage), getMultiple(rightPage));
+
+  return Math.max(highestLeftPage, highestRightPage);
+}
+
+function checkHasException(pages) {
+  const [leftPage, rightPage] = pages;
+
+  if (leftPage === 1 || rightPage === 400) return true;
+  if (Math.abs(leftPage - rightPage) > 1) return true;
+
+  return false;
+}
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (checkHasException(pobi) || checkHasException(crong)) return -1;
+
+  const pobiNumber = getHighestNumber(pobi);
+  const crongNumber = getHighestNumber(crong);
+
+  if (pobiNumber > crongNumber) return 1;
+  else if (pobiNumber < crongNumber) return 2;
+  else if (pobiNumber === crongNumber) return 0;
 }
 
 module.exports = problem1;
